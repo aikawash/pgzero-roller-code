898d5e1dfa65fd0f1569cd2032969e7323dd17d3
Aika Washington <aikawashington867@gmail.com>
2022-03-06 15:01:18 -0500

    added buttons png

---
+++ file1.py
@@ -0,0 +1,16 @@
+import pgzrun
+import pgzero
+
+button1 = Actor('1button')
+button1.pos = 192, 270
+
+
+
+WIDTH = 1920
+HEIGHT = 1080
+
+
+def draw():
+        screen.fill((245, 102, 0))
+
+pgzrun.go()
